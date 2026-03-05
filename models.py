dc84fc97f3b331f9761056942829c49fdb55515b
Dypcet15 <jambhaleshravani13@gmail.com>
2026-03-05 18:49:07 +0530

    Add models.py containing LogisticRegressionModel, RandomForestModel, XGBoostModel, and EnsembleModel classes

---
+++ models.py
@@ -0,0 +1,51 @@
+class LogisticRegressionModel:
+    def __init__(self):
+        pass
+
+    def train(self, X_train, y_train):
+        pass
+
+    def predict(self, X_test):
+        pass
+
+    def evaluate(self, y_true, y_pred):
+        pass
+
+class RandomForestModel:
+    def __init__(self):
+        pass
+
+    def train(self, X_train, y_train):
+        pass
+
+    def predict(self, X_test):
+        pass
+
+    def evaluate(self, y_true, y_pred):
+        pass
+
+class XGBoostModel:
+    def __init__(self):
+        pass
+
+    def train(self, X_train, y_train):
+        pass
+
+    def predict(self, X_test):
+        pass
+
+    def evaluate(self, y_true, y_pred):
+        pass
+
+class EnsembleModel:
+    def __init__(self):
+        pass
+
+    def train(self, models, X_train, y_train):
+        pass
+
+    def predict(self, X_test):
+        pass
+
+    def evaluate(self, y_true, y_pred):
+        pass
